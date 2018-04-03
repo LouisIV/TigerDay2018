9c4550675870a3c09aef641ff7f1fb314af45de9
LouisIV <louislombardoiv@gmail.com>
2018-04-03 15:23:06 -0700

    Added link to help page

--- src/Test.js
+++ src/Test.js
@@ -236,7 +236,7 @@ class Test extends Component {
             <img src={logo} style={{height: 50}} alt="Pacific Logo"></img>
           </div>
           <div style={{flex: 1, display: 'flex', height: 50, justifyContent:'center', alignItems:'center', borderRadius: 2.5, backgroundColor: "var(--green)", marginLeft: 5}}>
-            <a href='https://www.google.com' style={{flex: 1,}}><label style={{color: 'white'}}>Help?</label></a>
+            <a href='https://www.google.com' target='_blank' style={{flex: 1,}}><label style={{color: 'white'}}>Help?</label></a>
           </div>
         </div>
       </div>
